2427f5f01810f972387eb405d19d3327eb160aec
Anurag Anand <anuraganand@Anurags-MacBook-Air.local>
2021-04-29 20:37:58 +0530

    signup removed

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ import Community from './components/Community';
 import Soil from './components/Soil';
 import Crop from './components/Crop';
 import Location from './components/Location';
-import Login from './components/Login';
 import Schemes from './components/Schemes';
 import ModernFarming from './components/ModernFarming';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -84,11 +83,6 @@ class App extends React.Component {
           <Route path="/crop">
             <AppNavbar />
             <Crop/>
-            <Footer />
-          </Route>
-          <Route path="/login">
-            <AppNavbar />
-            <Login/>
             <Footer />
           </Route>
         </Switch>
