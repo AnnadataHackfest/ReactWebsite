bc379ad05879b3bd96e4c074d82df6760bb4c4e6
Yash Vardhan <yashvardhan513@gmail.com>
2021-04-30 11:16:16 +0530

    answer a particular question function

--- src/components/Infos.js
+++ src/components/Infos.js
@@ -3,10 +3,28 @@ import {Container, ListGroup, ListGroupItem, Button} from "reactstrap";
 import {CSSTransition, TransitionGroup} from "react-transition-group";
 import { connect } from "react-redux";
 import { getQuestions } from "../actions/questionActions";
+import { addInfo } from "../actions/infoActions";
 import PropTypes from "prop-types";
 import Alert from 'react-bootstrap/Alert'
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
 
 class Infos extends React.Component {
+
+  state = {
+    modal: false,
+    question: "",
+    answer: "",
+    name: "",
+    phone: ""
+  }
 
     static propTypes = {
         getQuestions: PropTypes.func.isRequired,
@@ -16,6 +34,40 @@ class Infos extends React.Component {
 
   componentDidMount() {
     this.props.getQuestions();
+  }
+
+  toggle = () => {
+    this.setState({
+        modal: !this.state.modal
+    });
+  }
+
+  handleClick = (e) => {
+    this.setState({
+      question: e.target.value
+    });
+    this.toggle();
+  }
+
+  onChange = (e) => {
+      this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+      e.preventDefault();
+
+      const newInfo = {
+         question: this.state.question,
+         answer: this.state.answer,
+         name: this.state.name,
+         phone: this.state.phone
+      }
+
+      // Add item via addItem action
+
+      this.props.addInfo(newInfo);
+
+      this.toggle();
   }
   
   render() {
@@ -33,7 +85,7 @@ class Infos extends React.Component {
                  </p>
                  <hr />
                  <div className="d-flex justify-content-start">
-                  <a href="/"><Button color="primary">Answer now</Button>{' '}</a>
+                  <Button color="primary" onClick={this.handleClick} value={question}>Answer now</Button>{' '}
                  </div>
                  <div className="d-flex justify-content-end">
                   <a href={"tel:"+`${phone}`}><Button variant="outline-success">
@@ -44,6 +96,66 @@ class Infos extends React.Component {
                 ))}
                     
                 </div>
+
+                <Modal 
+                 isOpen={this.state.modal}
+                 toggle={this.toggle}
+                >
+                <ModalHeader toggle={this.toggle}>Answer the question</ModalHeader>
+                <ModalBody>
+                    <Form onSubmit={this.onSubmit}>
+                        <FormGroup>
+
+                            <Label for="item">Question</Label>
+                            <Input 
+                             type="text"
+                             name="question"
+                             id="question"
+                             placeholder="What you wanna talk about"
+                             value={this.state.question}
+                             style={{marginBottom: "1rem"}}
+                             disabled
+                            />
+
+                            <Label for="item">Answer</Label>
+                            <Input 
+                             type="text"
+                             name="answer"
+                             id="answer"
+                             placeholder="Propose your solution"
+                             onChange={this.onChange}
+                             style={{marginBottom: "1rem"}}
+                            />
+
+                            <Label for="item">Your Name</Label>
+                            <Input 
+                             type="text"
+                             name="name"
+                             id="name"
+                             placeholder="Your name"
+                             onChange={this.onChange}
+                             style={{marginBottom: "1rem"}}
+                            />
+
+                            <Label for="item">Phone</Label>
+                            <Input 
+                             type="text"
+                             name="phone"
+                             id="phone"
+                             placeholder="For further commuication"
+                             onChange={this.onChange}
+                             style={{marginBottom: "1rem"}}
+                            />
+
+                            <Button
+                             color="dark"
+                             style={{marginTop: "2rem"}}
+                             block
+                            >Submit</Button>
+                        </FormGroup>
+                    </Form>
+                </ModalBody>
+            </Modal>
         </Container>
       );
   }
@@ -56,4 +168,4 @@ const mapStateToProps = (state) => ({
     isAuthenticated: state.auth.isAuthenticated
 });
 
-export default connect(mapStateToProps, { getQuestions })(Infos);
+export default connect(mapStateToProps, { getQuestions, addInfo })(Infos);
