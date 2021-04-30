144c10358025fbec26547453dde66bbfd58ff827
Yash Vardhan <yashvardhan513@gmail.com>
2021-04-30 20:00:55 +0530

    add the check fire functionality

--- src/components/Location.js
+++ src/components/Location.js
@@ -26,10 +26,14 @@ class Location extends React.Component {
         lat: "",
         lng: "",
         soil_temp: "",
-        soil_moist: ""
+        soil_moist: "",
+        confidence: "",
+        frp: "",
+        detect_time: ""
       }
 
       this.onSubmit = this.onSubmit.bind(this);
+      this.onSubmitFire = this.onSubmitFire.bind(this);
   }
   
 
@@ -69,6 +73,28 @@ class Location extends React.Component {
           console.log('Data could not be fetched ', error);
         });
   }
+
+  onSubmitFire = (e) => {
+    const url = `https://api.ambeedata.com/latest/fire?lat=${this.state.lat}&lng=${this.state.lng}`;
+    axios({
+      method: 'GET',
+      url: url,
+      headers: {
+        "x-api-key": "vbOFm1KGNhaoAIQM0KrSPaknrXIgMaoQdXSMKsd5"
+      }
+    })
+      .then(response => {
+        console.log(response.data);
+        this.setState({
+            confidence: response.data.data[0].confidence,
+            frp: response.data.data[0].frp,
+            detect_time: response.data.data[0].detection_time
+        });
+      })
+      .catch(error => {
+        console.log('Data could not be fetched ', error);
+      });
+}
   
   static propTypes = {
       getSoil: PropTypes.func.isRequired,
@@ -92,7 +118,13 @@ class Location extends React.Component {
     Just surf around the corners and get a lot of useful information
   </p>
 </Alert>
-          <Button color="primary" onClick={this.onSubmit} style={{marginTop: "3rem", marginBottom: "3rem"}}>Check Your Soil</Button>
+          <div style={{display: "flex", textAlign: "center"}}>
+          <Button color="primary" onClick={this.onSubmit} style={{marginTop: "3rem", marginBottom: "3rem", marginLeft:"1rem", marginRight:"1rem"}}>Check Your Soil</Button>
+          <Button color="primary" onClick={this.onSubmitFire} style={{marginTop: "3rem", marginBottom: "3rem", marginLeft:"1rem", marginRight:"1rem"}}>Check for Fire</Button>
+          <Button color="primary" onClick={this.onSubmit} style={{marginTop: "3rem", marginBottom: "3rem", marginLeft:"1rem", marginRight:"1rem"}}>Check Your Soil</Button>
+          <Button color="primary" onClick={this.onSubmit} style={{marginTop: "3rem", marginBottom: "3rem", marginLeft:"1rem", marginRight:"1rem"}}>Check Your Soil</Button>
+          </div>
+          
           {this.state.soil_temp!=="" ? 
 <Card style={{ width: '18rem', marginTop: "1rem", marginBottom: "2rem" }}>
   <Card.Img variant="top" src="https://images.unsplash.com/photo-1510844355160-2fb07bf9af75?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c29pbHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
@@ -108,6 +140,24 @@ class Location extends React.Component {
   </Card.Body>
 </Card>
           : null}
+
+          {this.state.confidence!=="" ? 
+<Card style={{ width: '18rem', marginTop: "1rem", marginBottom: "2rem" }}>
+  <Card.Img variant="top" src="https://images.unsplash.com/photo-1476455553758-5a8b16727e23?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njd8fGZpcmV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
+  <Card.Body>
+    <Card.Title>Fire Chances in your Area</Card.Title>
+    <hr />
+    <Card.Text>
+      The fire chances currently in your area is <strong>{this.state.confidence}. </strong>
+      The corresponding FRP(Fire Radiative Power) is estimated to be around <strong>{this.state.frp} MW </strong>.  
+      This data was recorded at the time <strong>{this.state.detect_time}. </strong>
+      You do not need to worry in case of fire as well. Just follow these best practices as given
+    </Card.Text>
+    <Button variant="primary">Explore Best Practices</Button>
+  </Card.Body>
+</Card>
+          : null}
+
           </Container>
         </div>
       );
